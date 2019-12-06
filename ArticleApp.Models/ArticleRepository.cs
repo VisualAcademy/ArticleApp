a20df0e6bb72a408aec2ebc841aec4efa504ea51
WebCampKR <webcamp@outlook.kr>
2019-12-07 07:32:38 +0900

    4. 리포지토리 패턴과 리포지토리 인터페이스

---
+++ ArticleApp.Models/ArticleRepository.cs
@@ -0,0 +1,42 @@
+﻿using Dul.Domain.Common;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ArticleApp.Models
+{
+    /// <summary>
+    /// Repository Class: ADO.NET 또는 Dapper 또는 Entity Framework Core 
+    /// </summary>
+    public class ArticleRepository : IArticleRepository
+    {
+        public Task<Article> AddArticleAsync(Article article)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteArticleAsync(int i)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Article> EditArticleAsync(Article article)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<PagingResult<Article>> GetAllAsync(int pageIndex, int pageSize)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Article> GetArticleByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Article>> GetArticlesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ArticleApp.Models/IArticleRepository.cs
@@ -0,0 +1,21 @@
+﻿using Dul.Domain.Common;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ArticleApp.Models
+{
+    /// <summary>
+    /// Repository Interface
+    /// </summary>
+    public interface IArticleRepository
+    {
+        Task<Article> AddArticleAsync(Article article);         // 입력
+        Task<List<Article>> GetArticlesAsync();                 // 출력
+        Task<Article> GetArticleByIdAsync(int id);              // 상세
+        Task<Article> EditArticleAsync(Article article);        // 수정
+        Task DeleteArticleAsync(int i);                         // 삭제
+
+        Task<PagingResult<Article>> GetAllAsync(
+            int pageIndex, int pageSize);                       // 페이징
+    }
+}
