cb66f638d1408b13d01d548035ce25bd311a7171
WebCampKR <webcamp@outlook.kr>
2019-12-07 05:56:29 +0900

    3. 최소 크기의 모델과 테이블 생성 그리고 로컬 데이터베이스 게시

---
+++ ArticleApp.Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArticleApp.Models
+{
+    /// <summary>
+    /// 게시판(Article) 모델 클래스: Articles 테이블과 일대일로 매핑 
+    /// </summary>
+    public class Article
+    {        
+        /// <summary>
+        /// 일련번호
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 제목
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+    }
+}
---
+++ ArticleApp.SqlServer/dbo/Tables/Articles.sql
@@ -0,0 +1,7 @@
+﻿-- 게시판 테이블
+CREATE TABLE [dbo].[Articles]
+(
+	[Id] Int Not Null Primary Key Identity(1, 1),	-- 일련번호
+	[Title] NVarChar(255) Not Null,					-- 제목
+)
+Go
