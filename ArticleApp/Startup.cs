ff461892b7449e57bfc0c0cd941440ce662b9269
WebCampKR <webcamp@outlook.kr>
2019-12-08 05:20:54 +0900

    9. 공통 클래스인 DbContext와 Repository 클래스에 대한 의존성 주입 관련 코드를 Startup 파일에 추가

--- ArticleApp/Startup.cs
+++ ArticleApp/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ArticleApp.Areas.Identity;
 using ArticleApp.Data;
+using ArticleApp.Models;
 
 namespace ArticleApp
 {
@@ -40,6 +41,21 @@ namespace ArticleApp
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
+
+            AddDependencyInjectionContainerForArticles(services);
+        }
+
+        /// <summary>
+        /// 게시판(Articles) 관련 의존성 주입 관련 코드만 따로 모아서 관리
+        /// </summary>
+        private void AddDependencyInjectionContainerForArticles(IServiceCollection services)
+        {
+            // ArticleAppDbContext.cs Inject: New DbContext Add
+            services.AddEntityFrameworkSqlServer().AddDbContext<ArticleAppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // IArticleRepository.cs Inject: DI Container에 서비스(리포지토리) 등록
+            services.AddTransient<IArticleRepository, ArticleRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
