5fe4f5575f394f61937cd229412f243ae6c6690f
RedPlus <redplus@redplus.net>
2020-04-04 09:40:21 +0900

    코드 정리

--- ArticleApp.Models/ArticleAppDbContext.cs
+++ ArticleApp.Models/ArticleAppDbContext.cs
@@ -8,24 +8,31 @@ namespace ArticleApp.Models
     /// </summary>
     public class ArticleAppDbContext : DbContext
     {
+        // Install-Package Microsoft.EntityFrameworkCore
         // Install-Package Microsoft.EntityFrameworkCore.SqlServer
+        // Install-Package Microsoft.EntityFrameworkCore.Tools
         // Install-Package Microsoft.EntityFrameworkCore.InMemory
         // Install-Package System.Configuration.ConfigurationManager
+        // Install-Package Microsoft.Data.SqlClient
+
         public ArticleAppDbContext()
         {
             // Empty
+            // ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
         }
 
         public ArticleAppDbContext(DbContextOptions<ArticleAppDbContext> options)
             : base(options)
         {
             // 공식과 같은 코드
+            // ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // 닷넷 프레임워크 기반에서 호출되는 코드 영역: 
             // App.config 또는 Web.config의 연결 문자열 사용
+            // 직접 데이터베이스 연결문자열 설정 가능
             if (!optionsBuilder.IsConfigured)
             {
                 string connectionString = ConfigurationManager.ConnectionStrings[
